0f2909d4aaa7ac5cf6fe68f57f2dac2d0e6d26f7
Ben Kuhl <benkuhl@gmail.com>
2015-08-08 12:11:25 -0400

    Moved categories

--- resources/views/partials/service-card.blade.php
+++ resources/views/partials/service-card.blade.php
@@ -2,10 +2,10 @@
     <img src="{{ $service->logo->url() }}"/>
     <h4><span>{{ $service->name }}</span></h4>
     <div class="description">
+        <div>Category: <a href="/browse/{{ $service->category->slug }}">{{ $service->category->name }}</a></div>
         @foreach($service->metas as $meta)
             <div>{{ $meta->label->name }}: {{ $meta->value }}</div>
         @endforeach
-        <div>Category: <a href="/browse/{{ $service->category->slug }}">{{ $service->category->name }}</a></div>
     </div>
     <a href="{{ $service->getRedirectUrl() }}" class="btn btn-danger">Check it out</a>
 </div>
